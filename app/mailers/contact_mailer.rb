d66f7f6c767920e9f1c90ac858fc24339ff27d0e
Christopher Davis <Cbdavis@outlook.com>
2017-11-08 23:32:27 +0000

    Added contact form mailer view

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,10 +2,10 @@ class ContactMailer < ActionMailer::Base
     default to: "Cbdavis@outlook.com"
     
   def contact_email(name, email, body)
-        @name - name 
-        @email - email
-        @body - body
-        
-    mail (from: email, subject: "Contact form Message")
+    @name = name 
+    @email = email
+    @body = body
+      
+    mail(from: email, subject: "Contact Form Message")
   end
 end
