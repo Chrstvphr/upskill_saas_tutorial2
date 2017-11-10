74ee8097654abca45df8d5dd7e34b4b297d0b597
Christopher Davis <Cbdavis@outlook.com>
2017-11-10 18:05:00 +0000

    Added mailer settings to environment file

--- config/environment.rb
+++ config/environment.rb
@@ -9,7 +9,7 @@ ActionMailer::Base.smtp_settings = {
     :address        => ENV["MAILGUN_SMTP_SERVER"],
     :user_name      => ENV["MAILGUN_SMTP_LOGIN"],
     :password       => ENV["MAILGUN_SMTP_PASSWORD"],
-    :domain         => "warm-oasis-26009.herokuapp.com",
+    :domain         => 'warm-oasis-26009.herokuapp.com',
     :authentication => :plain,
 }
 ActionMailer::Base.delivery_method = :smtp
